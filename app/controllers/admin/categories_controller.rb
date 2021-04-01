3790d7c9f2d675358bf7c1aac92ad3c560f4a179
Joseph Blais <josephpierreblais@gmail.com>
2021-04-01 11:32:59 -0400

    Add validation for blank category creation

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,4 +1,6 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'];
+  
   def index
   @categories = Category.all
   end
@@ -11,10 +13,19 @@ class Admin::CategoriesController < ApplicationController
     @category = Category.new(category_params)
 
     if @category.save
-      redirect_to_to [:admin, :categories],
+      redirect_to [:admin, :categories],
       notice: 'Category created!'
     else
       render :new
     end
   end
+
+  private
+
+  def category_params
+    params.require(:category).permit(
+    :name
+  )
+  end
+  
 end
