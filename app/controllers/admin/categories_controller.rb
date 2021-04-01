ace0485ee56a38475530ccca58eb0b77851ebe52
Joseph Blais <josephpierreblais@gmail.com>
2021-04-01 10:34:43 -0400

    Add partial for category

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,2 +1,20 @@
 class Admin::CategoriesController < ApplicationController
+  def index
+  @categories = Categories.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to_to [:admin, :categories],
+      notice: 'Category created!'
+    else
+      render :new
+    end
+  end
 end
